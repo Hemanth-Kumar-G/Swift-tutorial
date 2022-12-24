e873476ea966bc274f3e3f89f9570e7a5b4ca421
hemanth <hemanth@vyaparapp.in>
2022-12-24 21:09:05 +0530

    Initial Commit

---
+++ days100/days100App.swift
@@ -0,0 +1,17 @@
+//
+//  days100App.swift
+//  days100
+//
+//  Created by HEMANTH on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct days100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
