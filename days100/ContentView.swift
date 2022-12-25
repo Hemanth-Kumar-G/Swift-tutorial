52b7e7c851a1dc7e2bb2a8aa0650185e28e9d9e8
hemanth <hemanth@vyaparapp.in>
2022-12-25 06:51:32 +0530

    feat : practise

--- days100/ContentView.swift
+++ days100/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var count=0
+    @State var textValue="abc"
+    
+    let list = ["hemanth","ajay","jnanesh","abc"]
+    @State var selectedValue = "jnanesh"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            
+            Form {
+                
+                Section("Nodi swami"){
+                    Picker("selected value ", selection:$selectedValue ){
+                        ForEach(list, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Text("Hello, world!")
+                
+                Button("count \(count) \(textValue)"){
+                    count += 1
+                }
+                
+                TextField("enter something", text:$textValue)
+                
+                ForEach(0..<10) {
+                    Text("\($0)")
+                    
+                }
+            }
+            .navigationTitle("Title")
+            .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.large)
         }
-        .padding()
+        
+        
     }
 }
 
