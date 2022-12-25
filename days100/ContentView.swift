7f114cb423cdfa911c32fd1e1776aa350b19a5e1
hemanth <hemanth@vyaparapp.in>
2022-12-25 08:39:58 +0530

    feat : totalAmount per person includes tips

--- days100/ContentView.swift
+++ days100/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 100.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 5
+    
+    @FocusState private var amountIsFocused : Bool
+    
+    let tipPercentages = [5,10,20]
+    
+    var totalPerPerson :Double{
+        //calculate the total per person  here with tips
+        
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grantTotal  = checkAmount + tipValue
+        let amountPerPeron = grantTotal / peopleCount
+        return amountPerPeron
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter the Currency",value: $checkAmount,
+                              format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("no of peoples" ,selection: $numberOfPeople){
+                        ForEach(2..<10){
+                            Text("\($0) no of people")
+                        }
+                    }
+                }
+                
+                Section{
+                    
+                    
+                    Picker("Tips percentage" ,selection: $tipPercentage){
+                        ForEach(tipPercentages,id: \.self){
+                            Text($0,format:.percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("how much tips you want to leave")
+                }
+                
+                
+                Section{
+                    Text(totalPerPerson,
+                         format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+            }
+            .navigationTitle("We Spilt")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done "){
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
